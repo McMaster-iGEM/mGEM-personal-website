6f2c5a96a5007bb1769bd7c6d7ca28391eb9de04
vanessa-laii <vanessa.sw.lai@gmail.com>
2024-06-14 12:33:56 -0400

    updated footer

--- app/(home)/SubPages/AboutUs.tsx
+++ app/(home)/SubPages/AboutUs.tsx
@@ -6,6 +6,30 @@ const AboutUs = () => {
             <div className="text-4xl font-bold text-left mt-32 mb-5">
                 ABOUT US
             </div>
+            <section className="stats mt-[50px] xl:mt-[100px] relative z-20 maroon py-[50px] xl:py-[100px] text-white">
+                <div className="container mx-auto space-y-10">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                        <div className="stats__item text-center md:border-l-0 border-white">
+                            <div className="inline-block">
+                                <h1 className="text-5xl font-bold mb-4">40+</h1>
+                                <p className="text-xl">Members</p>
+                            </div>
+                        </div>
+                        <div className="stats__item text-center border-white md:border-l md:border-r-0">
+                            <div className="inline-block">
+                                <h1 className="text-5xl font-bold mb-4">10+</h1>
+                                <p className="text-xl">Years Established</p>
+                            </div>
+                        </div>
+                        <div className="stats__item text-center md:border-l md:border-r-0 border-white">
+                            <div className="inline-block">
+                                <h1 className="text-5xl font-bold mb-4">2nd</h1>
+                                <p className="text-xl">Place in 2023</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,17 +1,22 @@
 import { SiGithub } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-white p-8">
-      <hr className="my-8 border-blue-gray-50" />
-      <div className="flex flex-col justify-center items-center py-4">
-        <h1 color="blue-gray" className="text-center font-normal">
-          &copy; 2024 McMaster iGEM
-        </h1>
-        <a href="https://github.com/vanessa-laii/mGEM-personal-website" aria-label="Github">
-          <SiGithub className="w-5 h-5 hover:scale-125 transition-all mt-2" />
-        </a>
+    <footer>
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 mt-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+              <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                  <img src="MGEM-Logo.png" className="h-8" alt="Flowbite Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">mGEM</span>
+              </Link>
+              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <p className = "text-xs">McMaster iGEM recognizes and acknowledges that it is located on the traditional territories of the <br/> Mississauga and Haudenosaunee nations, and within the lands protected by the “Dish with One Spoon” wampum agreement.</p>
+              </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">mGEM™</Link>. All Rights Reserved.</span>
       </div>
-    </footer>
+  </footer>
   );
 }
