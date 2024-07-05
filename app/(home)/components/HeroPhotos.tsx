1c6690d7a3e51c865fc62d3fd84966c6ab122c24
vanessa-laii <vanessa.sw.lai@gmail.com>
2024-07-04 23:21:57 -0400

    add photos to hero section

--- app/(home)/components/HeroPhotos.tsx
+++ app/(home)/components/HeroPhotos.tsx
@@ -10,83 +10,32 @@ export default function Storybook() {
   );
 }
 
-const SkeletonOne = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Cool Photo ;-;</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  );
-};
 
-const SkeletonTwo = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Cool Photo ;-;</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  );
-};
-const SkeletonThree = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Cool Photo ;-;</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  );
-};
-const SkeletonFour = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">Cool Photo ;-;</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  );
-};
+
 
 const cards = [
   {
     id: 1,
-    content: <SkeletonOne />,
     className: "md:col-span-2",
     thumbnail:
-    "/MGEM-Logo.png",
+    "/WetLab_TeamPhoto.jpg",
   },
   {
     id: 2,
-    content: <SkeletonTwo />,
     className: "col-span-1",
     thumbnail:
-    "/MGEM-Logo.png",
+    "/Westdale_Workshop.jpg",
   },
   {
     id: 3,
-    content: <SkeletonThree />,
     className: "col-span-1",
     thumbnail:
-    "/MGEM-Logo.png",
+    "/HP_Workshop.jpg",
   },
   {
     id: 4,
-    content: <SkeletonFour />,
     className: "md:col-span-2",
     thumbnail:
-    "/MGEM-Logo.png",
+    "/JamboreePhoto.png",
   },
 ];
